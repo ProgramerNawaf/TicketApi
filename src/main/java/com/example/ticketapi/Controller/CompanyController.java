c4f4e7cdec4bd61d9bb5cf019af67452ecfe46f4
mohammed <malajaji.97@gmail.com>
2023-05-31 11:56:50 +0300

    Company All Good :)

--- src/main/java/com/example/ticketapi/Controller/CompanyController.java
+++ src/main/java/com/example/ticketapi/Controller/CompanyController.java
@@ -1,10 +1,17 @@
 package com.example.ticketapi.Controller;
 
 
+import com.example.ticketapi.Model.Company;
+import com.example.ticketapi.Model.Event;
 import com.example.ticketapi.Service.CompanyService;
+import jakarta.validation.Valid;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("api/v1/company")
@@ -12,6 +19,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class CompanyController {
 
     private final CompanyService companyService;
+
+    @GetMapping("/get")
+    public ResponseEntity getCompanies(){
+        List<Company> companies = companyService.getCompanies();
+        return ResponseEntity.status(200).body(companies);
+    }
+
+    @PostMapping("/add/{userId}")
+    public ResponseEntity addCompany(@Valid @RequestBody Company company, @PathVariable Integer userId){
+        companyService.addCompany(company,userId);
+        return ResponseEntity.status(200).body("Company Added");
+    }
+
+    @PutMapping("/update/{userId}")
+    public ResponseEntity updateCompany(@Valid @RequestBody Company company, @PathVariable Integer userId){
+        companyService.updateCompany(company,userId);
+        return ResponseEntity.status(200).body("Company Updated");
+    }
+
+    @DeleteMapping ("/delete/{companyId}/{userId}")
+    public ResponseEntity updateCompany(@PathVariable Integer companyId , @PathVariable Integer userId){
+        companyService.deleteCompany(companyId, userId);
+        return ResponseEntity.status(200).body("Company Updated");
+    }
+
+    @GetMapping("/events/{companyName}")
+    public ResponseEntity getCompanyEvents(@PathVariable String companyName){
+       Set<Event> events = companyService.getCompanyEvents(companyName);
+        return ResponseEntity.status(200).body(events);
+    }
 
 
 }
