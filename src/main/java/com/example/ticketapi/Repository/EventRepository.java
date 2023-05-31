b345332940d07efc7a4f8b8d1cfcbc473ca392e6
Nawaf Alahmed <TR3187@aecl.com>
2023-05-31 19:51:42 +0300

    company names

--- src/main/java/com/example/ticketapi/Repository/EventRepository.java
+++ src/main/java/com/example/ticketapi/Repository/EventRepository.java
@@ -1,5 +1,6 @@
 package com.example.ticketapi.Repository;
 
+import com.example.ticketapi.Model.Company;
 import com.example.ticketapi.Model.Event;
 import com.example.ticketapi.Model.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,4 +14,5 @@ public interface EventRepository extends JpaRepository<Event,Integer> {
     Event findEventById(Integer id);
 //    List<Ticket> findEventByTicketsContains(Event e);
     Event findEventByName(String name);
+    List <Event> findEventsByCompany(Company company);
 }
--- src/main/java/com/example/ticketapi/Service/CompanyService.java
+++ src/main/java/com/example/ticketapi/Service/CompanyService.java
@@ -6,6 +6,7 @@ import com.example.ticketapi.Model.Company;
 import com.example.ticketapi.Model.Event;
 import com.example.ticketapi.Model.MyUser;
 import com.example.ticketapi.Repository.CompanyRepository;
+import com.example.ticketapi.Repository.EventRepository;
 import com.example.ticketapi.Repository.MyUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,7 @@ import java.util.Set;
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
+    private final EventRepository eventRepository;
     private final MyUserRepository myUserRepository;
     public List<Company> getCompanies(){
         return companyRepository.findAll();
@@ -64,6 +66,17 @@ public class CompanyService {
             throw new ApiException("Company Not Found");
         }
         return company.getEvents();
+    }
+    public List<String> getCompanyNames(Integer adminId){
+        List<Company> company = companyRepository.findAll();
+        if (company == null){
+            throw new ApiException("Company Not Found");
+        }
+        List<String> compnayNames= null;
+
+        for(int i = 0 ; i<company.size();i++)
+            compnayNames.add(company.get(i).getName());
+        return compnayNames;
     }
 
 
