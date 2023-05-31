08e093749c6fcd023bd9feeaeb4f11546d981205
Mohammed Alabdulhadi <mohammad588577@gmail.com>
2023-05-31 16:10:32 +0300

    add endpoint checkValid

--- src/main/java/com/example/ticketapi/Service/TicketService.java
+++ src/main/java/com/example/ticketapi/Service/TicketService.java
@@ -5,8 +5,13 @@ import com.example.ticketapi.Model.Event;
 import com.example.ticketapi.Model.Ticket;
 import com.example.ticketapi.Repository.EventRepository;
 import com.example.ticketapi.Repository.TicketRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 
 @Service
@@ -16,15 +21,17 @@ public class TicketService {
     private final TicketRepository ticketRepository;
     private final EventRepository eventRepository;
 
-    public String checkValidTicket(Integer idTicket, String name) {
+    public String checkValidTicket(Integer idTicket, String nameEvent) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
         String message;
         Ticket ticket = ticketRepository.findTicketById(idTicket);
-        Event event = eventRepository.findEventByName(name);
+        Event event = eventRepository.findEventByName(nameEvent);
+        System.out.println(ticket.getEvent().getDate());
         if (ticket.getEvent().getId() == event.getId()) {
             return message = "Valid ticket";
         }
         return message = "Invalid ticket";
-
     }
 
 }
